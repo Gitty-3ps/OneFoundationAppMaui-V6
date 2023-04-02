04130ed58cd6729a1ce69c9067f563a26e623f64
Deron Hutchinson <89865124+Gitty-3ps@users.noreply.github.com>
2023-04-02 15:06:21 -0500

    feat (MVVM and CRUD) create database service

---
+++ OneFoundationAppMaui/Models/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneFoundationAppMaui.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Authors { get; set; }
+
+        public string Lyrics { get; set; }
+    }
+}
---
+++ OneFoundationAppMaui/Services/SongApiService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OneFoundationAppMaui.Services
+{
+    public class SongApiService
+    {
+        HttpClient _httpClient;
+        public static string BaseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:8097" :
+            "http://localhost:8099";
+
+        public string StatusMessage;
+        public SongApiService()
+        {
+            _httpClient = new() { BaseAddress = new Uri(BaseAddress) };
+        }
+
+        //public async Task <List<SongApiService>>GetSongs
+    }
+}
